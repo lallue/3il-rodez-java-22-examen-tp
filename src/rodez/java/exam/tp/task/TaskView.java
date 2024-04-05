20ab4811d81f282663ac8142e596364443d74f40
lallue <alluel@3il.fr>
2024-04-05 15:01:46 +0200

    ajout de la vue

---
+++ src/rodez/java/exam/tp/task/TaskView.java
@@ -0,0 +1,83 @@
+package rodez.java.exam.tp.task;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+
+import javax.swing.*;
+
+public class TaskView extends JFrame{
+	private JTextField titreField = new JTextField(20);
+	private JTextArea descriptionArea= new JTextArea(5,20);
+	private JTextField dateEcheanceField = new JTextField(20);
+	private TaskControlleur controlleur;
+	
+	private JButton addButton = new JButton("Ajout Tache");
+	
+	public void TaskView(TaskControlleur controlleur){
+		this.controlleur = controlleur;
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400,300);	
+		
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints  constaints = new GridBagConstraints();
+		constaints.gridx = 0;
+		constaints.gridy = 0;
+		constaints.anchor = GridBagConstraints.WEST;
+		
+		panel.add(new JLabel("Titre"), constaints);
+		
+		constaints.gridy = 1;
+		panel.add(new JLabel("Description"), constaints);
+		
+		constaints.gridy = 2;
+		panel.add(new JLabel("Date echeance jour/moi/annee"), constaints);
+		
+		constaints.gridx = 1;
+		
+		constaints.gridy = 0;
+		panel.add(titreField, constaints);
+		
+		constaints.gridy = 1;
+		panel.add(descriptionArea, constaints);
+		
+		constaints.gridy = 2;
+		panel.add(dateEcheanceField, constaints);
+		
+		constaints.gridy = 3;
+		constaints.anchor = GridBagConstraints.CENTER;
+		
+		addButton.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+			    String titre = titreField.getText();
+			    String description = descriptionArea.getText();
+			    String date = dateEcheanceField.getText();
+			    
+			    Date duedate = parseDate(date);
+			    
+			    Task task = new Task(titre, description, duedate);
+			    task.setTitre(titre);
+			    task.setDescription(description);
+			    task.setDateEcheance(duedate);
+			    
+			    controlleur.addTask(task);
+			    
+			    titreField.setText("");
+			    descriptionArea.setText("");
+			    dateEcheanceField.setText("");
+			  }
+			} );
+		this.add(panel);
+		this.setVisible(true);
+	}
+
+	protected Date parseDate(String date) {
+		
+		return null;
+	}
+	public static void main(String[] args) {
+		TaskControlleur conttroller = new TaskControlleur();
+		new TaskView();
+	}
+}
